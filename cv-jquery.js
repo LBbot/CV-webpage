998e902543cbd5451047b48b6897c5495557e02c
LBbot <LBbot@users.noreply.github.com>
2017-08-19 20:38:12 +0100

    initial commit

---
+++ cv-jquery.js
@@ -0,0 +1,14 @@
+function main() {
+			$("#main").hide();
+			$("#main").fadeIn(1500);
+
+			$(".content").hide();
+
+			$("h3").on("click", function() {
+
+			$(this).next().slideToggle(500);
+			$(this).toggleClass("active");
+				});
+			}
+
+$(document).ready(main);
